5a0a9660b380de885528ffbf5696e52724543c7f
Nikesh Chaudhari <info.nikesh12@gmail.com>
2025-03-08 20:39:51 +0545

    todo

---
+++ myapp/src/App.js
@@ -0,0 +1,19 @@
+import '../src/App.css'
+import Todo from './component/Todo';
+
+const App = ()=>{
+return(
+  <>
+  <div className="main-container">
+<div className="center-container"> 
+<Todo/>
+
+</div>
+
+  </div>
+  
+  
+  </>
+)
+}
+export default App;
