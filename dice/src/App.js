bafd1abeb5970f5d75b4f3394981d61715879191
Nikesh Chaudhari <info.nikesh12@gmail.com>
2025-03-11 22:53:31 +0545

    dice-roll

---
+++ dice/src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import './App.css'
+import Dice from './Dice'
+const App = () => {
+ 
+  return (
+    <div>
+      <Dice/>
+    </div>
+  )
+}
+
+export default App
